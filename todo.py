9da01e9b4bd3a0092f55ee124896f1462dd7108c
Sergii Biletskyi <sergii.biletskyi@tulip.com>
2025-10-19 14:13:32 -0600

    Add input validation.

--- todo.py
+++ todo.py
@@ -11,9 +11,9 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-l','--list', action="store_true",help='Display todo list')
 parser.add_argument('-d','--delete',metavar=' ', help='Delete item from todo list')
-parser.add_argument('-a','--add', nargs=4, metavar=' ',help='Add todo item to the list. Accepts 4 arguments: todo,priority,status,due date')
+parser.add_argument('-a','--add', nargs=None, metavar=' ',help='Add todo item to the list. Accepts 4 arguments: todo,priority,status,due date')
 parser.add_argument('-c','--complete', nargs = 1, metavar=' ',help='Comlete todo item')
-parser.add_argument('-e','--edit', nargs=1, metavar='', help='Edit todo item')
+parser.add_argument('-e','--edit', nargs=1, metavar='', type=int, help='Edit todo item. Enter the list number you want to edit')
 parser.add_argument('-s','--sort', nargs=1, metavar='', type=sort_argument_checker, help='Sort todo list by a column. Accepts values "p" - priority; "s" - status; "d" - due date')
 
 args = parser.parse_args()
@@ -34,12 +34,14 @@ class Todo_List:
         }
         with open(self.todo_file, mode='r') as f:
             data = json.load(f)
-        sorted_list = sorted(data['resources'], key=lambda r: r[fields[field[0]]], reverse=False)
-        sorted_dict = {
-            'resources' : sorted_list
-        }
-        with open(self.todo_file, mode='w') as f:
-            json.dump(sorted_dict, f, indent=2)
+            sorted_list = sorted(data['resources'], key=lambda r: r[fields[field[0]]], reverse=False)
+            sorted_dict = {
+                'resources' : sorted_list
+            }
+        count = 1
+        for todo in sorted_dict['resources']:
+            print(f"[{count}] \u23f5 {todo['todo']} | {todo['priority']} | {todo['status']} | {todo['due_date']}")
+            count += 1
 
     def display_todo_list(self):
         with open(self.todo_file, 'r') as f:
@@ -51,11 +53,15 @@ class Todo_List:
 
 
     def delete_item(self, n):
-        with open(self.todo_file, mode='r') as f:
+        with open(self.todo_file, mode='r+') as f:
             data = json.load(f)
-        with open(self.todo_file, mode='w') as f:
-            del data['resources'][n-1]
-            json.dump(data, f)
+            try:
+                del data['resources'][n-1]
+                json.dump(data, f)
+            except:
+                print("There's no such todo item")
+        # with open(self.todo_file, mode='r+') as f:
+
 
     def add_item(self, todo, priority, status, due_date):
         data = {
@@ -73,23 +79,26 @@ class Todo_List:
     def complete_item(self, n):
         with open(self.todo_file, mode='r') as f:
             data = json.load(f)
-        data['resources'][n-1]['status'] = "Completed"
-        with open(self.todo_file, mode='w') as f:
-            json.dump(data, f, indent=2)
-    def edit_item(self, n, todo='', priority='', status='', due_date=''):
-        with open(self.todo_file, mode='r') as f:
-            data = json.load(f)
-        if len(todo) > 0:
-            data['resources'][n-1]['todo'] = todo
-        if len(priority) > 0:
-            data['resources'][n-1]['priority'] = priority
-        if len(status) > 0:
-            data['resources'][n-1]['status'] = status
-        if len(due_date) > 0:
-            data['resources'][n-1]['due_date'] = due_date
-        
-        with open(self.todo_file, mode='w') as f:
-            json.dump(data, f, indent=2)
+            if n > len(data['resources']):
+                print("You are trying to complete non-existing todo item. Enter valid number: \n")
+                self.display_todo_list()
+            else:
+                with open(self.todo_file, mode='w') as f:
+                    data['resources'][n-1]['status'] = "Completed"
+                    json.dump(data, f, indent=2)
+
+    def edit_item(self, n, data, todo='', priority='', status='', due_date=''):       
+                if len(todo) > 0:
+                    data['resources'][n-1]['todo'] = todo
+                if len(priority) > 0:
+                    data['resources'][n-1]['priority'] = priority
+                if len(status) > 0:
+                    data['resources'][n-1]['status'] = status
+                if len(due_date) > 0:
+                    data['resources'][n-1]['due_date'] = due_date
+                
+                with open(self.todo_file, mode='w') as f:
+                    json.dump(data, f, indent=2)
         
 
 todo_list = Todo_List()
@@ -101,25 +110,29 @@ elif args.delete:
     todo_list.delete_item(to_delete)
     todo_list.display_todo_list()
 elif args.add:
-    todo = args.add[0]
-    priority = args.add[1]
-    status = args.add[2]
-    due_date = args.add[3]
+    todo = input("Todo: ")
+    priority = input("Priority: ")
+    status = input("Status: ")
+    due_date = input("Due Date: ")
     todo_list.add_item(todo, priority, status, due_date)
     todo_list.display_todo_list()
 elif args.complete:
     todo_list.complete_item(int(args.complete[0]))
     todo_list.display_todo_list()
 elif args.edit:
-    todo = input("Todo: ")
-    priority = input("Priority: ")
-    status = input("Status: ")
-    due_date = input("Due_date: ")
-
-    todo_list.edit_item(int(args.edit[0]), todo, priority, status, due_date)
-    todo_list.display_todo_list()
+     with open(todo_list.todo_file, mode='r') as f:
+        data = json.load(f)
+        if args.edit[0] > len(data['resources']):
+            print("There's no such todo item. Please enter valid number: \n")
+            todo_list.display_todo_list()
+        else:
+            todo = input("Todo: ")
+            priority = input("Priority: ")
+            status = input("Status: ")
+            due_date = input("Due_date: ")
+            todo_list.edit_item(int(args.edit[0]), data, todo, priority, status, due_date)
+            todo_list.display_todo_list()        
 elif args.sort:
     todo_list.sort_todo_list(args.sort)
-    todo_list.display_todo_list()
 
 
